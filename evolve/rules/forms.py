bfdb98a8ed1150d10e03a76d3c4ae9ddab069af0
Daniel F Moisset <dmoisset@machinalis.com>
2011-10-22 19:57:56 -0300

    Correction on a dict key for a form

--- evolve/rules/forms.py
+++ evolve/rules/forms.py
@@ -12,7 +12,7 @@ class EffectForm(forms.ModelForm):
         # This is done here instead of model validation because kinds_scored is M2M,
         # so it's not yet set during model validation
         has_score_1 = bool(cleaned_data['kinds_scored'])
-        has_score_2 = cleaned_data['score_per_local_building'] > 0 or cleaned_data['self.score_per_neighbor_building'] > 0
+        has_score_2 = cleaned_data['score_per_local_building'] > 0 or cleaned_data['score_per_neighbor_building'] > 0
         if has_score_1 != has_score_2:
             raise forms.ValidationError('Set kind_scored attribute along score_per_*')
         return cleaned_data

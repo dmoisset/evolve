d09ba5842192346d54260eeff0c4514a9a2abd01
Daniel F Moisset <dmoisset@machinalis.com>
2011-10-22 22:26:44 -0300

    Base template
    Including jquery.ui

--- evolve/settings.py
+++ evolve/settings.py
@@ -56,7 +56,8 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+import os.path
+STATIC_ROOT = os.path.join(os.path.dirname(__file__), 'static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"

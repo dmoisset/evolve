525bd6f5124333adf0d53d4f544af966ee6e6833
Daniel F Moisset <dmoisset@machinalis.com>
2011-10-24 22:46:29 -0300

    free_having is not symmetrical!

--- evolve/rules/models.py
+++ evolve/rules/models.py
@@ -386,7 +386,7 @@ class Building(models.Model):
     effect = models.ForeignKey(Effect)
 
     cost = models.ForeignKey(Cost)
-    free_having = models.ManyToManyField('self', blank=True, null=True) # This models is free when having other bulding
+    free_having = models.ManyToManyField('self', blank=True, null=True, symmetrical=False) # This models is free when having other bulding
 
     def score(self, local, left, right):
         """
